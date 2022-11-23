c1a4e2f5621a9fa39f9e07618f40f14e8d247c18
Frédéric Bury <82781574+FredericBury@users.noreply.github.com>
2022-11-23 18:44:17 +0100

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Définir une constante N, qui vaudra 10 pour ce début du TP, et un type Tab tableau de N entiers
+#define N 1000
+typedef int Tab[N];
+

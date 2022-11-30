d85e8d2521374f8a19d572ef3ee9575a576d75cd
Frédéric Bury <82781574+FredericBury@users.noreply.github.com>
2022-11-30 15:47:36 +0100

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define MAX 10
+typedef char Tab[MAX];
+
+
+typedef struct Client
+{
+    int id;
+    char nom[15];
+}Client;
+
